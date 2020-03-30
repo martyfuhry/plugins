a7ab1f8b2b4c826d902be3bbd5d1f17ec1eb7ec3
Marty Fuhry <marty@reportallusa.com>
2020-03-30 13:58:03 -0400

    fixed a bug where maps initialized in iOS with a tileoverlay already set weren't adding their tile overlay in the constructor

--- packages/google_maps_flutter/google_maps_flutter/ios/Classes/GoogleMapController.m
+++ packages/google_maps_flutter/google_maps_flutter/ios/Classes/GoogleMapController.m
@@ -118,6 +118,10 @@ static double ToDouble(NSNumber* data) { return [FLTGoogleMapJsonConversions toD
     if ([circlesToAdd isKindOfClass:[NSArray class]]) {
       [_circlesController addCircles:circlesToAdd];
     }
+    id tileOverlaysToAdd = call.arguments[@"tileOverlaysToAdd"];
+    if ([tileOverlaysToAdd isKindOfClass:[NSArray class]]) {
+      [_tileOverlaysController addTileOverlays:tileOverlaysToAdd];
+    }
   }
   return self;
 }
