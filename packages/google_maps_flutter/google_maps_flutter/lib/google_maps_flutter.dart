2f481bef42be786ac69b318813370ff52620631b
Marty Fuhry <marty@reportallusa.com>
2020-10-23 13:47:46 -0400

    added types

--- packages/google_maps_flutter/google_maps_flutter/lib/google_maps_flutter.dart
+++ packages/google_maps_flutter/google_maps_flutter/lib/google_maps_flutter.dart
@@ -44,7 +44,10 @@ export 'package:google_maps_flutter_platform_interface/google_maps_flutter_platf
         PolygonId,
         Polyline,
         PolylineId,
-        ScreenCoordinate;
+        ScreenCoordinate,
+        Tile,
+        TileProvider,
+        TileOverlay;
 
 part 'src/controller.dart';
 part 'src/google_map.dart';
