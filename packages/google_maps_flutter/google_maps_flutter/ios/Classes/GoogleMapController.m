64222605cfc54a0a8e264944384347130655c1a7
Marty Fuhry <marty@reportallusa.com>
2021-02-18 11:43:48 -0500

    fixed padding for iOS

--- packages/google_maps_flutter/google_maps_flutter/ios/Classes/GoogleMapController.m
+++ packages/google_maps_flutter/google_maps_flutter/ios/Classes/GoogleMapController.m
@@ -69,6 +69,7 @@ static double ToDouble(NSNumber* data) { return [FLTGoogleMapJsonConversions toD
     GMSCameraPosition* camera = ToOptionalCameraPosition(args[@"initialCameraPosition"]);
     _mapView = [GMSMapView mapWithFrame:frame camera:camera];
     _mapView.accessibilityElementsHidden = NO;
+    _mapView.paddingAdjustmentBehavior = kGMSMapViewPaddingAdjustmentBehaviorNever;
     _trackCameraPosition = NO;
     InterpretMapOptions(args[@"options"], self);
     NSString* channelName =
